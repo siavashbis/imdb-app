635fa55f7dc1ba1b9736df531b669dc10c7895c9
siavashbis <siavashbis@gmail.com>
2025-11-19 13:56:44 +0330

    added card and results

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
-export default function Home() {
+import Results from '@/components/Results';
+const API_KEY = process.env.API_KEY;
+export default async function Home() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
   return (
-    <div className='text-red-600'>Home</div>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
--- src/app/top/[genre]/page.jsx
+++ src/app/top/[genre]/page.jsx
@@ -1,5 +1,21 @@
-export default function TopPages() {
+import Results from '@/components/Results';
+const API_KEY = process.env.API_KEY;
+export default async function Home({ params }) {
+  const genre = params.genre;
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'rated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+
   return (
-    <div>TopPages</div>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
